eb743147ad471af208a24713a1ecbcc5b5149b3a
Anton <60780638+antonio63963@users.noreply.github.com>
2024-09-20 17:07:58 +0300

    front:added start letters in Avatar if no img

--- client/src/common/components/Avatar/Avatar.tsx
+++ client/src/common/components/Avatar/Avatar.tsx
@@ -4,17 +4,19 @@ import cn from "classnames";
 import "./Avatar.css";
 
 interface IAvatar {
-  img?: string;
+  img?: string | null;
   isOnline?: boolean;
+  name?: string;
 }
 
-const Avatar: FC<IAvatar> = ({ img, isOnline }) => {
+const Avatar = ({ img, isOnline, name }: IAvatar) => {
+  console.log(name?.slice(0, 2))
   return (
     <div className={cn("avatarItemWrapper", isOnline ? "online" : null)}>
       <div
         className={cn("avatarItem")}
-        style={{ backgroundImage: "url('/users/albert.jpg')" }}
-      ></div>
+        style={{ backgroundImage: `url(${img})` }}
+      >{!img && name ? name.slice(0, 2) : null}</div>
       <div className={cn("onlineIndicator")}></div>
     </div>
   );
