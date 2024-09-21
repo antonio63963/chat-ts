081c071c179d7eb78ab94733099210f93d296642
Anton <60780638+antonio63963@users.noreply.github.com>
2024-09-21 18:05:03 +0300

    front:created NoConversagtion component

---
+++ client/src/Pages/HomePage/components/NotSelectedUser/NotSelectedConversation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotSelectedConversation =() => {
+  return (
+    <div className='notSelectedUser'>
+      <img src="/svg/callService.svg" alt="not selected conversation" />
+    </div>
+  )
+}
+
+export default NotSelectedConversation;
