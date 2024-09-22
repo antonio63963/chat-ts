4fb910d6a956826358710240624820cf1f278bf7
Anton <60780638+antonio63963@users.noreply.github.com>
2024-09-22 12:33:22 +0300

    front:created SearchInput

---
+++ client/src/common/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { MagnifyingGlass } from "phosphor-react";
+
+import './SearchInput.css';
+
+interface ISearchInput {
+  value: string;
+  onChange: (data: string) => void;
+}
+
+function SearchInput({ value, onChange }: ISearchInput) {
+  return (
+    <div className="searchInput lightBgSecondary">
+      <MagnifyingGlass size={16} />
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Search..."
+      />
+    </div>
+  );
+}
+
+export default SearchInput;
