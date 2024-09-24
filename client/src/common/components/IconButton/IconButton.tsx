5d6804a55936d5cb7d7d713e12df532414ea500c
Anton <60780638+antonio63963@users.noreply.github.com>
2024-09-24 10:23:40 +0300

    front:added filledButton as option IconButton

--- client/src/common/components/IconButton/IconButton.tsx
+++ client/src/common/components/IconButton/IconButton.tsx
@@ -1,17 +1,23 @@
-import React, { ReactNode } from 'react';
-import './IconButton.css';
+import React, { ReactNode } from "react";
+import cn from "classnames";
+
+import "./IconButton.css";
 
 interface IIconButton {
   children: ReactNode;
   onClick: () => void;
-};
+  isFilled?: boolean;
+}
 
-function IconButton({children, onClick}: IIconButton) {
+function IconButton({ children, onClick, isFilled }: IIconButton) {
   return (
-    <button onClick={onClick} className='iconButton'>
+    <button
+      onClick={onClick}
+      className={cn(isFilled ? "filledIconButton" : "iconButton")}
+    >
       {children}
     </button>
-  )
-};
+  );
+}
 
-export default IconButton
+export default IconButton;
